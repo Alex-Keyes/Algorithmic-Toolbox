c09cb1d946b5ec18e83a5b8c4a60491a2e942735
Alex Keyes <alex.o.keyes@gmail.com>
2018-02-08 15:18:54 -0500

    add solution to max pairwise product

--- week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
+++ week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.py
@@ -3,11 +3,8 @@ n = int(input())
 a = [int(x) for x in input().split()]
 assert(len(a) == n)
 
-result = 0
+max1 = max(a)
+a.remove(max(a))
+max2 = max(a)
 
-for i in range(0, n):
-    for j in range(i+1, n):
-        if a[i]*a[j] > result:
-            result = a[i]*a[j]
-
-print(result)
+print(max1 * max2)
