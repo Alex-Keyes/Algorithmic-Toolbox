5a587c32a8ad0bdd4afdc07c1f8819de2a2f4d0c
Alex Keyes <alex.o.keyes@gmail.com>
2018-02-18 18:29:31 -0500

    add fib sum last digit

--- week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.py
+++ week2_algorithmic_warmup/6_last_digit_of_the_sum_of_fibonacci_numbers/fibonacci_sum_last_digit.py
@@ -1,21 +1,28 @@
 # Uses python3
-import sys
+f = [0] * 1000
 
-def fibonacci_sum_naive(n):
-    if n <= 1:
-        return n
 
-    previous = 0
-    current  = 1
-    sum      = 1
+def fib(n):
+    n = int(n)
+    # Base cases
+    if (n == 0):
+        return 0
+    if (n == 1 or n == 2):
+        return (1)
+    # If fib(n) is already computed
+    if (f[n] == True):
+        return f[n]
+    k = (n+1)/2 if (n & 1) else n/2
+    # Applying above formula [Note value n&
+    # is 1 if n is odd, else 0].
+    f[n] = (fib(k) * fib(k) + fib(k-1) * fib(k-1)) if (n & 1) else (2 * fib(k-1) + fib(k)) * fib(k)
+    return f[n] 
 
-    for _ in range(n - 1):
-        previous, current = current, previous + current
-        sum += current
 
-    return sum % 10
+# Computes value of first Fibonacci numbers
+def calculateSum(n):
+    return fib(n+2) - 1
 
-if __name__ == '__main__':
-    input = sys.stdin.read()
-    n = int(input)
-    print(fibonacci_sum_naive(n))
+
+n = int(input())
+print(calculateSum(n))
