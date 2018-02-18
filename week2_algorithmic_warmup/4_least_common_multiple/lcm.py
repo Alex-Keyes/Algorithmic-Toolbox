5ed918d5b305612ba8ede4edd6fba24b05014243
Alex Keyes <alex.o.keyes@gmail.com>
2018-02-18 11:58:49 -0500

    add solution for least common multiple

--- week2_algorithmic_warmup/4_least_common_multiple/lcm.py
+++ week2_algorithmic_warmup/4_least_common_multiple/lcm.py
@@ -1,15 +1,13 @@
 # Uses python3
-import sys
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
 
-def lcm_naive(a, b):
-    for l in range(1, a*b + 1):
-        if l % a == 0 and l % b == 0:
-            return l
 
-    return a*b
+def lcm_efficient(a, b):
+    return int(a * b / gcd(a, b))
 
-if __name__ == '__main__':
-    input = sys.stdin.read()
-    a, b = map(int, input.split())
-    print(lcm_naive(a, b))
 
+a, b = map(int, input().split())
+print(lcm_efficient(a, b))
