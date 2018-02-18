1d126773a76f8060c3b5098008ec609f858705c4
Alex Keyes <alex.o.keyes@gmail.com>
2018-02-18 16:38:27 -0500

    add solution fib_huge

--- week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge.py
+++ week2_algorithmic_warmup/5_fibonacci_number_again/fibonacci_huge.py
@@ -1,19 +1,24 @@
 # Uses python3
-import sys
+def get_fibonacci(n):
+    fib_nums = [0, 1]
+    for num in range(2, n+1):
+        fib_nums.append(fib_nums[num-1] + fib_nums[num-2])
+    return fib_nums[n]
 
-def get_fibonacci_huge_naive(n, m):
-    if n <= 1:
-        return n
 
+def fib_period_length(m):
     previous = 0
-    current  = 1
+    current = 1
+    for i in range(m * m + 1):
+        previous, current = current, (previous + current) % m
+        if previous == 0 and current == 1:
+            return i + 1
 
-    for _ in range(n - 1):
-        previous, current = current, previous + current
 
-    return current % m
+def get_fibonacci_huge_fast(n, m):
+    remainder = n % fib_period_length(m)
+    return get_fibonacci(remainder) % m
 
-if __name__ == '__main__':
-    input = sys.stdin.read();
-    n, m = map(int, input.split())
-    print(get_fibonacci_huge_naive(n, m))
+
+n, m = map(int, input().split())
+print(get_fibonacci_huge_fast(n, m))
